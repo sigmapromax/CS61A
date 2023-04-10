e9419e1bd89afecb7f82dfe33bc98b6f273b8c3c
Zhang Linfeng <zhanglinfengcs@outlook.com>
2023-04-10 20:36:33 +0800

    disc: 05 finished

---
+++ disc/disc05/disc05.py
@@ -0,0 +1,169 @@
+# ====================== Running in 61A Code ======================
+
+# Q2: Height
+def height(t):
+    """Return the height of a tree.
+
+    >>> t = tree(3, [tree(5, [tree(1)]), tree(2)])
+    >>> height(t)
+    2
+    >>> t = tree(3, [tree(1), tree(2, [tree(5, [tree(6)]), tree(1)])])
+    >>> height(t)
+    3
+    """
+    if is_leaf(t):
+        return 0
+    else:
+        max_h = 0
+        for b in branches(t):
+            max_h = max(height(b), max_h)
+        return max_h + 1
+
+# Q3: Maximum Path Sum
+def max_path_sum(t):
+    """Return the maximum path sum of the tree.
+
+    >>> t = tree(1, [tree(5, [tree(1), tree(3)]), tree(10)])
+    >>> max_path_sum(t)
+    11
+    """
+    if is_leaf(t):
+        return 1
+    else:
+        max_p = 0
+        for b in branches(t):
+            max_p = max(max_path_sum(b), max_p)
+        return max_p + 1
+
+# Q4: Find Path
+def find_path(t, x):
+    """
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])] ), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10)  # returns None
+    """
+    if label(t) == x:
+        return [label(t)]
+    for b in branches(t):
+        path = find_path(b, x)
+        if path:
+            return [label(t)] + path
+        
+# Q5: Map, Filter, Reduce
+def my_map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    """
+    return [fn(i) for i in seq]
+
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    """
+    return [i for i in seq if pred(i)]
+
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    total = seq.pop(0)
+    for i in seq:
+        total = combiner(total, i)
+    return total
+
+# Q6: Count palindromes
+def count_palindromes(L):
+    """The number of palindromic words in the sequence of strings
+    L (ignoring case).
+
+    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
+    2
+    """
+    return len([i for i in L if i.lower() == i.lower()[::-1]])
+
+# Q7: Perfectly Balanced
+def sum_tree(t):
+    """
+    Add all elements in a tree.
+    >>> t = tree(4, [tree(2, [tree(3)]), tree(6)])
+    >>> sum_tree(t)
+    15
+    """
+    if is_leaf(t):
+        return label(t)
+    else:
+        sum = label(t)
+        for b in branches(t):
+            sum += sum_tree(b)
+        return sum
+    
+
+def balanced(t):
+    """
+    Checks if each branch has same sum of all elements and
+    if each branch is balanced.
+    >>> t = tree(1, [tree(3), tree(1, [tree(2)]), tree(1, [tree(1), tree(1)])])
+    >>> balanced(t)
+    True
+    >>> t = tree(1, [t, tree(1)])
+    >>> balanced(t)
+    False
+    >>> t = tree(1, [tree(4), tree(1, [tree(2), tree(1)]), tree(1, [tree(3)])])
+    >>> balanced(t)
+    False
+    """
+    sum = -1
+    for b in branches(t):
+        if sum == -1:
+            sum = sum_tree(b)
+        else:
+            if sum != sum_tree(b):
+                return False
+    return True
+
+# Q8: Hailstone Tree
+def hailstone_tree(n, h):
+    """Generates a tree of hailstone numbers that will reach N, with height H.
+    >>> print_tree(hailstone_tree(1, 0))
+    1
+    >>> print_tree(hailstone_tree(1, 4))
+    1
+        2
+            4
+                8
+                    16
+    >>> print_tree(hailstone_tree(8, 3))
+    8
+        16
+            32
+                64
+            5
+                10
+    """
+    if h == 0:
+        return tree(n)
+    else:
+        branches = [hailstone_tree(n * 2, h - 1)]
+        if (n - 1) % 3 == 0 and ((n-1) // 3) % 2 == 1 and (n-1) // 3 > 1:
+            branches += [hailstone_tree((n-1)//3, h-1)]
+        return tree(n, branches)
+
+def print_tree(t):
+    def helper(i, t):
+        print("    " * i + str(label(t)))
+        for b in branches(t):
+            helper(i + 1, b)
+    helper(0, t)
+
+
