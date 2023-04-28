d67a5cf7f90f7a6a33afd0f941147da13bee8a59
Zhang Linfeng <zhanglinfengcs@outlook.com>
2023-04-28 21:14:38 +0800

    lab: 10 finished q1-q2

---
+++ labs/lab10/lab10.scm
@@ -0,0 +1,18 @@
+(define (over-or-under num1 num2) 
+  (if (< num1 num2)
+    -1
+    (
+      if (= num1 num2)
+      0
+      1
+    )
+  )
+)
+
+(define (make-adder num) 'YOUR-CODE-HERE)
+
+(define (composed f g) 'YOUR-CODE-HERE)
+
+(define lst 'YOUR-CODE-HERE)
+
+(define (remove item lst) 'YOUR-CODE-HERE)
